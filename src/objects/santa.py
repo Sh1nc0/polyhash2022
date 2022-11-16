2cf02c145c1b021cf1cb1c72475b5c3209dadbad
Lilian FORGET <lilian.forget1@etu.univ-nantes.fr>
2022-11-16 16:00:59 +0100

    feat(santa.py): mise en place de la classe santa

--- src/objects/santa.py
+++ src/objects/santa.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from deliveryPoint import DeliveryPoint
+from math import sqrt
+
 class Santa:
 
     def __init__(self) :
-        self.vx : int = 0
-        self.vy : int = 0
-        self.weight : int = 0
-        # self.dictV : ?
-        self.carrots : int = 0
-        self.loadedGifts : dict[int, int] = {}
+        self.x : int = 0                                #Position en X
+        self.vx : int = 0                               #Vitesse en X
 
-    def getDistance(self, dv) -> float :
-        pass
+        self.y : int = 0                                #Position en Y
+        self.vy : int = 0                               #Vitesse en Y
+
+        self.weight : int = 0                           #Poids du traineau
+        self.maxSpeed : list[tuple[int,int]] = []       #Palier de vitesse
+        self.carrots : int = 0                          #Nombres de carottes
+
+        self.loadedGifts : list[DeliveryPoint] = []     #Cadeau chargé (représenté par leur Delivery Point)
+
+    def getDistance(self, dv : DeliveryPoint) -> float :
+        return abs(sqrt((self.x - dv.x) ** 2 + (self.y - dv.y) ** 2))
 
     def getMaxAcc(self) -> int:
-        pass
-
-    def setVx(self, newVx : int) :
-        self.vx = newVx
-
-    def setVy(self, newVy : int) :
-        self.vy = newVy
-
-    def setWeight(self, newWeight : int) :
-        self.weight = newWeight
+        for v in self.maxSpeed :
+            weight, speed = v
+            if weight > self.weight :
+                return speed
