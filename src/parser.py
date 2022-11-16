eebf927c5dfb5b28a822c92a63c8cc030f531f78
Romain Pipon <romain.pipon@etu.univ-nantes.fr>
2022-11-16 20:53:10 +0100

    refactor(parser.py): Remplacement de la fonction par une classe et nettoyage du code

--- src/parser.py
+++ src/parser.py
@@ -2,35 +2,56 @@
 # -*- coding: utf-8 -*-
 
 from objects.deliveryPoint import DeliveryPoint
+from objects.santa import Santa
 
-"""Module de parsing des fichiers d'entrée pour la mise en oeuvre du projet Poly#.
-"""
+class Parser:
 
+    def __init__(self, filename: str):
+        self.filename: str = filename
 
-def parse_challenge(filename: str) -> object:
-    """Lit un fichier de challenge et extrait les informations nécessaires.
-    """
+        self.time: int = 0
+        self.deliveryDistance: int = 0
+        self.nbAcc: int = 0
+        self.nbGifts: int = 0
 
-    with open(filename, "r") as f:
-        lines = f.readlines()
+        self.santa: Santa = Santa()
+        self.deliveryPoints: list = []
 
-    config = {}
-    deliveryPoints = []
-    accRange = {}
+    def parse(self):
+        with open(self.filename, "r") as f:
+            lines = f.readlines()
 
-    for line in lines:
-        properties = line.split(" ")
-        if len(properties) == 4:  # config
-            config["time"] = int(properties[0])
-            config["deliveryDistance"] = int(properties[1])
-            config["nbAccRange"] = int(properties[2])
-            config["nbGifts"] = int(properties[3])
+        for line in lines:
+            properties = line.split(" ")
 
-        if len(properties) == 2:  # accRange
-            accRange[int(properties[0])] = int(properties[1])
+            if len(properties) == 4:
+                """
+                    Configuration
+                    properties[0]: time
+                    properties[1]: delivery distance
+                    properties[2]: number of acceleration steps
+                    properties[3]: number of gifts
+                """
+                self.time = int(properties[0])
+                self.deliveryDistance = int(properties[1])
+                self.nbAcc = int(properties[2])
+                self.nbGifts = int(properties[3])
 
-        if len(properties) == 5:  # DeliveryPoint
-            deliveryPoints.append(DeliveryPoint(
-                properties[0], properties[1], properties[2], properties[3], properties[4]))
+            if len(properties) == 2:
+                """ 
+                    Acceleration steps
+                    properties[0]: weight
+                    properties[1]: max speed
+                """
+                self.santa.maxSpeed.append((int(properties[0]), int(properties[1])))
 
-    return config, deliveryPoints, accRange
+            if len(properties) == 5:
+                """
+                    DeliveryPoint
+                    properties[0]: Name
+                    properties[1]: Score
+                    properties[2]: Weight
+                    properties[3]: X
+                    properties[4]: Y
+                """
+                self.deliveryPoints.append(DeliveryPoint(properties[0], int(properties[1]), int(properties[2]), int(properties[3]), int(properties[4])))
