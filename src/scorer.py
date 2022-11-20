35a0fb725ce83e6cd52b1a2b0a21107e365ae2ba
hippolyte_roussel <hippolyte.roussel@etu.univ-nantes.fr>
2022-11-20 14:27:58 +0100

    chore(src/scorer.py): creation d'un fichier scorer.py avec description en entête

---
+++ src/scorer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Hippolyte ROUSSEL hippolyte.roussel@etu.univ-nantes.fr
+# 20/11/22
+#
+# Vérifie que les mouvements dans le fichier parcours_généré suivent les règles imposées du défi suivant une  situation_de_depart. Fournit un score si tout est bon ou lance des exceptions si une règle n'est pas respectée.
+# Liste des règles : https://codingcompetitions.withgoogle.com/hashcode/round/00000000008cacc6/0000000000aff4a0 Section "Validation" (bas de page)
+#
+# INPUT:
+# situation_de_depart.txt // fichier des cadeaux à transmettre
+# parcours_généré.txt // fichier des mouvements du pere-noel pour remettre les différents cadeaux.
+# IMPORT interne au projet:
+# parser.py // de Romain
+# OUTPUT
+# score || Exception d'une règle non respecté
+
