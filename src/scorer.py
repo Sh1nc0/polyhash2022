873d7f2a2bc7a9bbd31b5e8ebb57330b11b2ae86
hippolyte_roussel <hippolyte.roussel@etu.univ-nantes.fr>
2022-11-25 08:20:43 +0100

    docs(src/scorer.py): UNCOMPLETE : Relecture et commentaire de code.

--- src/scorer.py
+++ src/scorer.py
@@ -9,46 +9,16 @@
 # INPUT:
 # challenge.txt // fichier des cadeaux à transmettre
 # parcours.txt // fichier des mouvements du pere-noel pour remettre les différents cadeaux.
-# IMPORT interne au projet:
-# parser.py // de Romain. Pour accéder aux données du challenge plus facilement.
+
 # OUTPUT
 # score:int || Exception d'une règle non respecté
 
-# parcours.txt à la forme de l'exemple suivant: (sans les commentaires)
-# 23
-# LoadCarrots 10
-# LoadGift Olivia
-# LoadGift Liam
-# AccRight 4
-# Float 1
-# DeliverGift Olivia
-# AccUp 2
-# Float 1
-# DeliverGift Liam
-# AccLeft 8
-# Float 1
-# AccDown 4
-# Float 1
-# LoadGift Bob
-# AccRight 4
-# Float 1
-# AccDown 6
-# Float 1
-# AccDown 6
-# Float 1
-# AccDown 6
-# Float 4
-# DeliverGift Bob
-
-
-import argparse
 from math import *
 import sys
 
-
-
-
-
+#CONSTANTES
+MASS_CARROT = 1 # Le sujet indique qu'une carotte pèse 1kg
+NUMBER_CARROT_CONSUMPTION_DURING_ACCELERATION = 1 # Le sujet ne précise pas si peut importe la pusissance de l'accélération, la consommation est de 1 ou si la consommation est proportionnelle à la puissance de l'accélération.
 
 class Gift: # A verifier mais name doit etre unique en théorie
     def __init__(self, name:str, score:int, weight:int, target_c:int, target_r :int):
@@ -61,84 +31,85 @@ class Gift: # A verifier mais name doit etre unique en théorie
     def __str__(self):
         return "Gift : ", self.name, " score : ", self.score, " weight : ", self.weight, " target_c : ", self.target_c, " target_r : ", self.target_r
 
-class Santa: # Entité qui va etre mise a jour durant l'execution du programme pour verifier que tout est bon.
+class Santa: # Entité qui va etre mise à jour durant l'exécution du programme pour vérifier que tout est bon.
     # Le santa a des ordres absolus:
     # Donc pas de vérification dans ses méthodes. 
     # Mais modification des variables internes dépendantes des méthodes. si acceleration -> carrot--
 
     def __init__(self) :
 
-        self.c : int = 0                                #Position en c ou X
-        self.r : int = 0                                #Position en r ou Y
+        self.position_c : int = 0                                #Position en c (ou aka X)
+        self.position_r : int = 0                                #Position en r (ou aka Y)
 
-        self.vc : int = 0                               #Vitesse en c ou X
-        self.vr : int = 0                               #Vitesse en r ou Y
+        self.velocity_c : int = 0                               #Vitesse en c (ou aka X)
+        self.velocity_r : int = 0                               #Vitesse en r (ou aka Y)
 
         self.weight : int = 0                           #Poids du traineau
         self.carrots : int = 0                          #Nombres de carottes
 
-        self.loadedGifts = {}              #loadedGifts Cadeau chargé liste
+        self.loadedGifts = {}                           #dictionnaire de cadeaux chargés
 
 
     def loadCarrots(self, nbCarrots : int) :
         self.carrots += nbCarrots
-        self.weight += nbCarrots * 1 # une carotte pèse 1kg
+        self.weight += nbCarrots * MASS_CARROT
     
     def loadGift(self, gift: Gift) :
         self.loadedGifts[gift.name] = gift
         self.weight += gift.weight
 
     def unLoadGift(self, gift_name: str) :
-        gift = self.loadedGifts[gift_name] # TODO delete element from dict
-        self.loadedGifts[gift_name] = None
-        self.weight -= gift.weight
+        gift = self.loadedGifts[gift_name] 
+        del self.loadedGifts[gift_name] # suppression de ce cadeau du dictionnaire
+        self.weight -= gift.weight # mise à jour du poids total du traineau vu qu'on a déchargé un cadeau
         return gift
 
-    def updatePosition(self) : #s'active à chaque tours
-        self.c += self.vc
-        self.r += self.vr
+    def updatePosition(self) : #s'active à chaque tours via la méthode self.FLoat()
+        self.position_c += self.velocity_c
+        self.position_r += self.velocity_r
 
-    def getMaxAccelerationForCurrentWeight(self, weight_acceleration_ratio) -> int: #retourne l'acceleration max possible selon le tableau en constante globale en fonction du poids total du traineau. TODO Ajouter le typage valide
-        for i in range(len(weight_acceleration_ratio)):
-            if self.weight >= weight_acceleration_ratio[i][0]:
-                return weight_acceleration_ratio[i][1]
-        return weight_acceleration_ratio[len(weight_acceleration_ratio)-1][1] #return l'acceleration du dernier element de la liste. equivaut l'acceleration maximale possible qui est dans la liste
+    # On cherche à connaitre l'accélération maximale possible selon le poids actuel du traineau. Les accélérations maximales en fonction du poids sont définies dans le tableau en paramètre.
+    def getMaxAccelerationForCurrentWeight(self, weight_acceleration_ratio: list(tuple(int,int))) -> int: 
+        for i in range(len(weight_acceleration_ratio)):                         # Pour chacun des couples(poids,accélération) de la liste
+            if self.weight >= weight_acceleration_ratio[i][0]:                  # Si le poids du traineau est supérieur ou égal au poids du couple:
+                return weight_acceleration_ratio[i][1]                          # On retourne l'accélération associée à ce couple
+        return weight_acceleration_ratio[len(weight_acceleration_ratio)-1][1]   # Dans le cas ou le poids est plus petit que la limitation la plus faible (celle en fin de tableau), on retourne cette dernière limitation.
 
-    # 4 fonction de changement d'acceleration
-    def accUp(self, nb : int) : # accélération sur r Y
-        self.vr += nb
-        self.carrots -= 1 # lors d'une acceration, on consomme une carotte peut importe la force de l'acceleration
-        self.weight -= 1
+    # Fonctions de changement d'accelerations
+    def accUp(self, acceleration_strength : int) : # accélération sur r (ou aka Y)
+        self.velocity_r += acceleration_strength                                                             # l'accélération est ajoutée à la vitesse
+        self.carrots -= NUMBER_CARROT_CONSUMPTION_DURING_ACCELERATION              # lors d'une acceration, on consomme une carotte peut importe la force de l'acceleration
+        self.weight -= MASS_CARROT * NUMBER_CARROT_CONSUMPTION_DURING_ACCELERATION # On consomme des carrotes donc la masse totale diminue.
     
-    def accDown(self, nb : int) : # deceleration sur r Y
-        self.vr -= nb
-        self.carrots -= 1 
-        self.weight -= 1
+    def accDown(self, acceleration_strength : int) : # deceleration sur -r (ou aka -Y)
+        self.velocity_r -= acceleration_strength
+        self.carrots -= NUMBER_CARROT_CONSUMPTION_DURING_ACCELERATION
+        self.weight -= MASS_CARROT * NUMBER_CARROT_CONSUMPTION_DURING_ACCELERATION
 
-    def accLeft(self, nb : int) : # acceleration sur c X
-        self.vc -= nb
-        self.carrots -= 1 
-        self.weight -= 1
+    def accLeft(self, acceleration_strength : int) : # acceleration sur -c (ou aka -X)
+        self.velocity_c -= acceleration_strength
+        self.carrots -= NUMBER_CARROT_CONSUMPTION_DURING_ACCELERATION
+        self.weight -= MASS_CARROT * NUMBER_CARROT_CONSUMPTION_DURING_ACCELERATION
 
-    def accRight(self, nb : int) : # deceleration sur c X
-        self.vc += nb
-        self.carrots -= 1 
-        self.weight -= 1
+    def accRight(self, acceleration_strength : int) : # deceleration sur c (ou aka X)
+        self.velocity_c += acceleration_strength
+        self.carrots -= NUMBER_CARROT_CONSUMPTION_DURING_ACCELERATION
+        self.weight -= MASS_CARROT * NUMBER_CARROT_CONSUMPTION_DURING_ACCELERATION
 
-    def Float(self, nb : int): # Met à jour le Santa nb fois
-        for i in range(nb) :
+    def floatX(self, time_sec : int): # "float" = délais de flotement dans le ciel en seconde. Met à jour la position du Santa X fois.
+        for i in range(time_sec) :
             self.updatePosition()
     
             
 
-    def toString(self) -> str:
+    def __str__(self) -> str:
         string = "SANTA : \n"
-        string += "position c : " + str(self.c) + "\n"
-        string += "position r : " + str(self.r) + "\n"
-        string += "velocity c : " + str(self.vc) + "\n"
-        string += "velocity r : " + str(self.vr) + "\n"
-        string += "number of carrots : " + str(self.carrots) + "\n"
-        string += "total weight : " + str(self.weight) + "\n"
+        string += "position_c : " + str(self.position_c) + "\n"
+        string += "position_r : " + str(self.position_r) + "\n"
+        string += "velocity_c : " + str(self.velocity_c) + "\n"
+        string += "velocity_r : " + str(self.velocity_r) + "\n"
+        string += "number_of_carrots : " + str(self.carrots) + "\n"
+        string += "total_weight : " + str(self.weight) + "\n"
         string += "gifts:\n"
         for gift in self.loadedGifts :
             string += "{gift.name} : {gift.score}pts, {gift.weight}kg, target_c={gift.target_c}, target_r={gift.target_r}\n"
@@ -147,35 +118,27 @@ class Santa: # Entité qui va etre mise a jour durant l'execution du programme p
 
 
 if __name__ == "__main__":
-    # # lire les arguments (challenge.txt,parcours.txt)
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("challenge", help="fichier de la situation de départ")
-    # parser.add_argument("parcours", help="fichier du parcours généré")   
-    # args = parser.parse_args()
-
-    # # lire les fichiers
-    # file_challenge = parser.parse_challenge(args.challenge)
-    # file_parcours = parser.parse_parcours(args.parcours)
-
-    # récupératino des arguments:
+   
+    # récupération des arguments du programme:
     file_path_challenge = sys.argv[1]
     file_path_parcours = sys.argv[2]
 
-    # lecture des fichiers
+    # lecture des fichiers définis en arguments:
+    file_challenge: file
+    file_parcours: file
     file_challenge = open(file_path_challenge, "r")
     file_parcours = open(file_path_parcours, "r")
-    num_lines = sum(1 for line in file_parcours)
-    file_parcours = open(file_path_parcours, "r")
-
-
+    num_lines = sum(1 for line in file_parcours) # On compte les lignes du fichier parcours maintenant 
+    file_parcours = open(file_path_parcours, "r") # On réouvre le fichier pour le parcourir à nouveau dans la suite du programme. Car on ne peut pas parcourir un fichier deux fois.
+    
     # RECUPERATION DES PARAMETRES DE LA SIMULATION
     #Parametres constants apres la lecture du challenge.
     max_time_sec:int = 0
     delivery_distance:int = 0
     acceleration_range:int = 0
     number_of_gifts:int = 0
-    weight_acceleration_ratio = [] #couples des accelerations en fonction du poids total du traineau. TODO : list[tuple[int,int]] = []
-    gifts = {} # TODO typage : list[Gift]
+    weight_acceleration_ratio: list(tuple(int,int)) = [] #couples des accelerations en fonction du poids total du traineau.
+    gifts: dict(Gift) = {} 
 
     # creation d'un santa
     santa = Santa()
