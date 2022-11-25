1bba5be0a78e94f3c11358fff7aeaa2565bbdcb4
Romain Pipon <romain.pipon@etu.univ-nantes.fr>
2022-11-25 10:55:50 +0100

    feat(objects/game.py): Génération du fichier de sortie

--- src/objects/game.py
+++ src/objects/game.py
@@ -4,10 +4,11 @@
 from objects.santa import Santa
 from objects.gift import Gift
 from parser import Parser
+import os
 
 class Game :
 
-    def __init__(self, p : Parser) :
+    def __init__(self, p : Parser, output_dist: str = "../data/output_data/output.txt") :
 
         if p is None :
             raise Exception
@@ -24,15 +25,22 @@ class Game :
 
         self.toDeliver : list[Gift] = p.gifts
 
+        self.dist : str = output_dist
+        self.outputString : list[str] = []
+
     def accelerate(self, nb : int, dir : str) :
         if dir.lower() == "up" :
             self.santa.vy += nb
+            self.outputString.append(f"AccUp {nb}\n")
         elif dir.lower() == "down" :
             self.santa.vy -= nb
+            self.outputString.append(f"AccDown {nb}\n")
         elif dir.lower() == "right" :
             self.santa.vx += nb
+            self.outputString.append(f"AccRight {nb}\n")
         elif dir.lower() == "left" :
             self.santa.vx -= nb
+            self.outputString.append(f"AccLeft {nb}\n")
         else :
             return
 
@@ -43,6 +51,8 @@ class Game :
 
     def floatX(self, nb : int) :
         self.timeCount += nb
+
+        self.outputString.append(f"Float {nb}\n")
         self.actionCount += 1
 
         for _ in nb :
@@ -52,6 +62,7 @@ class Game :
         self.santa.carrots += nbCarrots
         self.santa.weight += nbCarrots
         
+        self.outputString.append(f"LoadCarrots {nbCarrots}\n")
         self.actionCount += 1
 
     def loadGift(self, g : Gift) :
@@ -59,10 +70,21 @@ class Game :
         self.santa.loadedGifts.append(g)
         self.santa.weight += g.weight
 
+        self.outputString.append(f"LoadGift {g.name}\n")
         self.actionCount += 1
 
     def deliverGift(self, g : Gift) :
         self.santa.loadedGifts.remove(g)
         self.santa.weight -= g.weight
 
+        self.outputString.append(f"DeliverGift {g.name}\n")
         self.actionCount += 1
+
+    def finish(self) :
+        os.makedirs(name=os.path.dirname(self.dist), exist_ok=True)
+        with open(self.dist, 'w') as f :
+            f.write(f"{self.actionCount}\n")
+            
+            for line in self.outputString:
+                f.write(line)
+        f.close()
