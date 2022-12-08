df841358124b9013fb21c3f0346b6dc4df0d5a38
Romain Pipon <romain.pipon@etu.univ-nantes.fr>
2022-12-08 16:37:32 +0100

    refactor: Ajout d'un fichier dédié aux constantes

---
+++ src/util/constants.py
@@ -0,0 +1,2 @@
+MASS_CARROT = 1 # Le sujet indique qu'une carotte pèse 1kg
+NUMBER_CARROT_CONSUMPTION_DURING_ACCELERATION = 1 # Le sujet ne précise pas si peut importe la pusissance de l'accélération, la consommation est de 1 ou si la consommation est proportionnelle à la puissance de l'accélération.
