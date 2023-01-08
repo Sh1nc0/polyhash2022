1ba724a11580434340da42ee0ffac1c1effbefb2
Romain Pipon <romain.pipon@etu.univ-nantes.fr>
2023-01-08 18:20:11 +0100

    feat(polyhash.py): Algo par zone

--- src/polyhash.py
+++ src/polyhash.py
@@ -29,31 +29,20 @@ if __name__ == "__main__":
     args = args.parse_args()
 
     p = Parser(args.fileInput)
-    #p = Parser("/Users/romain/Desktop/polyhash2022/data/input_data/f_festive_flyover.in.txt")
     p.parse()
 
     g = Game(p)
 
     t = []
-    # for i in range(0, -250, -g.maxDeliveryDistance):
-    #     t.append((0, i)) 
-    #     t.append((i, 0))
-    #     t.append((i, i))
 
-    # for i in range(0, 250, g.maxDeliveryDistance):
-    #     t.append((0, i)) 
-    #     t.append((i, 0))
-    #     t.append((i, i))
-
-    mS = (-500, 500, -500, 500)
-    #mS = getMap(g)
+    mS = getMap(g)
     print(mS)
 
     for i in range(mS[0], mS[1], g.maxDeliveryDistance if g.maxDeliveryDistance else 1):
         for j in range(mS[2], mS[3], g.maxDeliveryDistance if g.maxDeliveryDistance else 1):
             t.append((i, j))
 
-    print("2nd Parts")
+    print("Analyse des différente zones")
     p=[]
     for i in t:
         score=0
@@ -67,13 +56,14 @@ if __name__ == "__main__":
     p.sort(key=lambda x: x[2]/(getDist(x[0], x[1], 0,0)+1), reverse=True)
     print(p)
     t=p
-    print("3rd Parts")
+
+    print("Livraison des cadeaux")
 
     for i in range(len(t)):
         g.toDeliver.sort(key=lambda x: getDist(x.x, x.y,t[i][0], t[i][1]))
         goTo(0, 0, g)
         print(f"{g.santa.x} {g.santa.y}")
-
+        g.loadCarrots(100)
         if g.santa.getDistance(0, 0) <= g.maxDeliveryDistance:
             for j in range(len(g.toDeliver)):
                 if getDist(g.toDeliver[0].x, g.toDeliver[0].y, t[i][0], t[i][1])<= g.maxDeliveryDistance :
@@ -92,23 +82,3 @@ if __name__ == "__main__":
 
     g.finish()
     print(f"score : {g.score}")
-
-
-    # posx,posy = 0,0
-
-    # for i in range(150):
-    #     g.toDeliver.sort(key=lambda x: getDist(x.x, x.y,(posx),(posy)))
-    #     goTo(0, 0, g)
-    #     g.loadCarrots(200)
-
-    #     for i in range(len(g.toDeliver)):
-    #         if getDist(g.toDeliver[0].x, g.toDeliver[0].y, posx, posy)<= g.maxDeliveryDistance :
-    #             g.loadGift(g.toDeliver[0])
-
-        
-    #     for i in range(len(g.santa.loadedGifts)):
-    #         goTo(g.santa.loadedGifts[0].x, g.santa.loadedGifts[0].y, g)
-    #         g.deliverGift(g.santa.loadedGifts[0])
-
-    #     posx -= 15
-    #     posy += 5
